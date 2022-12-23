3e54cd3b975fb28a1105cab2d644da72f63e1fff
Alexey Illarionov <alexey@0xdc.ru>
2022-12-23 19:24:48 +0300

    Move plugins from src/main/kotlin to src/

--- gradle/plugins/android/build.gradle.kts
+++ gradle/plugins/android/build.gradle.kts
@@ -1,11 +1,24 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
 plugins {
-    `kotlin-dsl`
+    `java-gradle-plugin`
+    `kotlin-dsl` apply false
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
+
+sourceSets {
+    main {
+        java.srcDir("src")
+    }
+}
+
+// Workaround for https://github.com/gradle/gradle/issues/21052 -
+// apply kotlin-dsl plugin last, because it erroneously fetches source dirs eagerly.
+apply(plugin = "org.gradle.kotlin.kotlin-dsl")
 
 dependencies {
     implementation(libs.agp.plugin)

84bfe0d6184edb6b14b95b51f9695c326d1f1bc1
Alexey Illarionov <alexey@0xdc.ru>
2022-12-27 17:44:38 +0300

    Specify exclusiveContent for the google repository.
    Add Intellij language injections for regexp arguments of RepositoryContentDescriptor in Gradle

--- gradle/meta-plugins/settings/convention/ru.pixnews.repositories.settings.gradle.kts
+++ gradle/meta-plugins/settings/convention/ru.pixnews.repositories.settings.gradle.kts
@@ -3,7 +3,7 @@
  */
 pluginManagement {
     repositories {
-        google()
+        googleExclusiveContent()
         gradlePluginPortal()
     }
 
@@ -19,7 +19,58 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        google()
+        googleExclusiveContent()
         mavenCentral()
+    }
+}
+
+fun RepositoryHandler.googleExclusiveContent(): Unit = exclusiveContent {
+    forRepository(::google)
+    filter {
+        // https://maven.google.com/web/index.html
+        includeGroupByRegex("""android\.arch\..*""")
+        includeGroupByRegex("""androidx\..+""")
+        includeGroupByRegex("""com\.android(?:\..+)?""")
+        includeGroupByRegex("""com\.crashlytics\.sdk\.android\..*""")
+        includeGroupByRegex("""com\.google\.ads\..*""")
+        includeGroupByRegex("""com\.google\.android\..*""")
+        listOf(
+            "com.google.ambient.crossdevice",
+            "com.google.androidbrowserhelper",
+            "com.google.ar",
+            "com.google.ar.sceneform",
+            "com.google.ar.sceneform.ux",
+            "com.google.assistant.appactions",
+            "com.google.assistant.suggestion",
+            "com.google.camerax.effects",
+            "com.google.chromeos",
+            "com.google.d2c",
+            "com.google.devtools.ksp",
+            "com.google.fhir",
+            "com.google.firebase",
+            "com.google.firebase.appdistribution",
+            "com.google.firebase.crashlytics",
+            "com.google.firebase.firebase-perf",
+            "com.google.firebase.testlab",
+            "com.google.gms",
+            "com.google.gms.google-services",
+            "com.google.jacquard",
+            "com.google.mediapipe",
+            "com.google.mlkit",
+            "com.google.net.cronet",
+            "com.google.oboe",
+            "com.google.prefab",
+            "com.google.relay",
+            "com.google.test.platform",
+            "com.google.testing.platform",
+            "io.fabric.sdk.android",
+            "tools.base.build-system.debug",
+            "zipflinger"
+        ).map(::includeGroup)
+
+        includeModuleByRegex(
+            """org\.jetbrains\.kotlin""",
+            """kotlin-ksp|kotlin-symbol-processing-api"""
+        )
     }
 }

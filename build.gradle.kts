a963b6bf6e17c53dcf65828e590ef79faf818ea4
Alexey Illarionov <alexey@0xdc.ru>
2022-12-28 18:40:53 +0300

    Add Gradle Versions Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.ben.manes.versions)
 }
 
 tasks.register<Delete>("clean") {

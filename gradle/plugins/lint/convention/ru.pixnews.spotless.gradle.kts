0459037cbf77783a461b7575ad0f196712b4dc7f
Alexey Illarionov <alexey@0xdc.ru>
2023-01-03 22:36:09 +0300

    Add license file

--- gradle/plugins/lint/convention/ru.pixnews.spotless.gradle.kts
+++ gradle/plugins/lint/convention/ru.pixnews.spotless.gradle.kts
@@ -1,29 +1,50 @@
+/*
+ * Copyright 2023 Alexey Illarionov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.diffplug.spotless.FormatterStep
+import com.diffplug.spotless.generic.LicenseHeaderStep
+import ru.pixnews.configRootDir
+
 /**
  * Convention plugin that configures Spotless
  */
-
-import ru.pixnews.configRootDir
-
 plugins {
     id("com.diffplug.spotless")
 }
 
 spotless {
+    isEnforceCheck = false
     kotlin {
         target("**/*.kt")
-        targetExclude("**/build/**/*.kt")
+        targetExclude("**/build/**/*.kt", "config/copyright/**")
 
         diktat().configFile(configRootDir.file("diktat.yml"))
+        licenseHeaderFile(configRootDir.file("copyright/copyright.kt"))
     }
     kotlinGradle {
         target("**/*.gradle.kts")
-        targetExclude("**/build/**/*.gradle.kts")
+        targetExclude("**/build/**/*.gradle.kts", "config/copyright/**")
 
         diktat().configFile(configRootDir.file("diktat.yml"))
+        licenseHeaderFile(configRootDir.file("copyright/copyright.kt"), "(^(?![\\/ ]\\*).*$)")
     }
     java {
         target("**/*.java")
-        targetExclude("**/build/**/*.java")
+        targetExclude("**/build/**/*.java", "config/copyright/**")
+        licenseHeaderFile(configRootDir.file("copyright/copyright.kt"))
 
         importOrder("", "javax", "java", "\\#")
 
@@ -34,8 +55,9 @@ spotless {
     }
     groovy {
         target("**/*.groovy")
-        targetExclude("**/build/**/*.groovy")
+        targetExclude("**/build/**/*.groovy", "config/copyright/**")
         importOrder("", "javax", "java", "\\#")
+        licenseHeaderFile(configRootDir.file("copyright/copyright.kt"))
 
         indentWithSpaces()
         trimTrailingWhitespace()
@@ -43,7 +65,8 @@ spotless {
     }
     groovyGradle {
         target("*.gradle")
-        targetExclude("**/build/**/*.gradle")
+        targetExclude("**/build/**/*.gradle", "config/copyright/**")
+        licenseHeaderFile(configRootDir.file("copyright/copyright.kt"), "(^(?![\\/ ]\\*).*$)")
 
         indentWithSpaces()
         trimTrailingWhitespace()
@@ -51,7 +74,7 @@ spotless {
     }
     format("yaml") {
         target("*.yml")
-        targetExclude("**/build/**/*.yml")
+        targetExclude("**/build/**/*.yml", "config/copyright/**")
 
         trimTrailingWhitespace()
         endWithNewline()
@@ -59,7 +82,7 @@ spotless {
     }
     format("markdown") {
         target("**/*.md", "**/*.markdown")
-        targetExclude("**/build/**/*.md", "**/build/**/*.markdown")
+        targetExclude("**/build/**/*.md", "**/build/**/*.markdown", "config/copyright/**")
 
         endWithNewline()
     }
@@ -70,9 +93,28 @@ spotless {
         clangFormat().style("LLVM")
         endWithNewline()
     }
-    format("misc") {
+    format("xml") {
         target("**/*.xml")
-        targetExclude("**/build/**/*.xml", ".idea/**/*.xml")
+        targetExclude("**/build/**/*.xml", ".idea/**/*.xml", "config/**", "gradle/verification-metadata.xml")
+
+        val deactivatableLicenseStep: FormatterStep = run {
+            val licenseHeaderFile = configRootDir.file("copyright/copyright.xml")
+            val licenseHeaderContentProvider = providers.fileContents(licenseHeaderFile).asText
+            val licenseHeaderFileStep = LicenseHeaderStep
+                .headerDelimiter(licenseHeaderContentProvider::get, "(<[^!?])")
+                .build()
+            object : FormatterStep by licenseHeaderFileStep {
+                override fun format(rawUnix: String, file: File): String? {
+                    return if (!rawUnix.contains("<!-- spotless: off -->")) {
+                        licenseHeaderFileStep.format(rawUnix, file)
+                    } else {
+                        rawUnix
+                    }
+                }
+            }
+        }
+        addStep(deactivatableLicenseStep)
+
         endWithNewline()
     }
 }

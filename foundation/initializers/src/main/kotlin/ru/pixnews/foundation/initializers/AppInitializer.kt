467f10951eecd17d4160b9b6aeafdaefed4c31b9
Alexey Illarionov <alexey@0xdc.ru>
2023-01-21 14:55:35 +0300

    Add initializers component using component dependencies, init app from app initializer (#12)

---
+++ foundation/initializers/src/main/kotlin/ru/pixnews/foundation/initializers/AppInitializer.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2023 Alexey Illarionov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ru.pixnews.foundation.initializers
+
+import androidx.tracing.Trace
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+
+public class AppInitializer(
+    private val initializers: Set<Initializer> = emptySet(),
+    private val asyncInitializers: Set<AsyncInitializer> = emptySet(),
+    asyncDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) {
+    private val scope: CoroutineScope = CoroutineScope(Job() + asyncDispatcher)
+
+    public fun init() {
+        startInitAsyncInitializers()
+        intSync()
+    }
+
+    private fun startInitAsyncInitializers() {
+        scope.launch {
+            Trace.beginAsyncSection(ASYNC_INIT_SECTION_NAME, 0)
+            try {
+                asyncInitializers.forEach {
+                    runTracing({ it.javaClass.simpleName }) {
+                        it.init()
+                    }
+                }
+            } finally {
+                Trace.endAsyncSection(ASYNC_INIT_SECTION_NAME, 0)
+            }
+        }
+    }
+
+    private fun intSync() {
+        Trace.beginSection(INIT_SECTION_NAME)
+        try {
+            initializers.forEach {
+                runTracing({ it.javaClass.simpleName }) {
+                    it.init()
+                }
+            }
+        } finally {
+            Trace.endSection()
+        }
+    }
+
+    private inline fun runTracing(lazyName: () -> String, initFunction: () -> Unit) {
+        if (Trace.isEnabled()) {
+            Trace.beginSection(lazyName())
+        }
+        try {
+            initFunction.invoke()
+        } finally {
+            if (Trace.isEnabled()) {
+                Trace.endSection()
+            }
+        }
+    }
+
+    public fun cancel() {
+        scope.cancel()
+    }
+
+    public companion object {
+        private const val INIT_SECTION_NAME: String = "AppInitializer init"
+        private const val ASYNC_INIT_SECTION_NAME = "AppInitializer async init"
+    }
+}

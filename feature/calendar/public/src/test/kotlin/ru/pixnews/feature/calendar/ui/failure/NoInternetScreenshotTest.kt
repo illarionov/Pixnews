898de90a386632ceb97a53aa50e1d0ce7d9320e1
Alexey Illarionov <alexey@0xdc.ru>
2023-10-25 12:59:18 +0300

    Add screenshot tests for failure states (#195)

---
+++ feature/calendar/public/src/test/kotlin/ru/pixnews/feature/calendar/ui/failure/NoInternetScreenshotTest.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023, the Pixnews project authors and contributors. Please see the AUTHORS file for details.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package ru.pixnews.feature.calendar.ui.failure
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams.RenderingMode.SHRINK
+import org.junit.Rule
+import org.junit.Test
+import ru.pixnews.foundation.ui.theme.PixnewsTheme
+
+class NoInternetScreenshotTest {
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.PIXEL_5.copy(
+            locale = "en",
+        ),
+        theme = "android:Theme.Material.Light.NoActionBar",
+        renderingMode = SHRINK,
+        showSystemUi = false,
+    )
+
+    @Test
+    fun noInternet() {
+        paparazzi.snapshot {
+            PixnewsTheme(useDynamicColor = false) {
+                NoInternet()
+            }
+        }
+    }
+}

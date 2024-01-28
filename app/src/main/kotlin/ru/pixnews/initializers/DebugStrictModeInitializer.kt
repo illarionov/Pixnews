4aed29b9ce7d79282fc320947f3505db734946d2
Alexey Illarionov <alexey@0xdc.ru>
2024-01-28 22:23:55 +0300

    Refactor strict mode listener

--- app/src/main/kotlin/ru/pixnews/initializers/DebugStrictModeInitializer.kt
+++ app/src/main/kotlin/ru/pixnews/initializers/DebugStrictModeInitializer.kt
@@ -5,7 +5,6 @@
 
 package ru.pixnews.initializers
 
-import android.os.Build
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy.Builder
 import android.os.StrictMode.VmPolicy
@@ -13,7 +12,9 @@ import androidx.fragment.app.strictmode.FragmentStrictMode
 import androidx.fragment.app.strictmode.FragmentStrictMode.Policy
 import co.touchlab.kermit.Logger
 import ru.pixnews.foundation.initializers.Initializer
-import java.util.concurrent.Executors
+import ru.pixnews.util.strictmode.ViolationPolicy.LOG
+import ru.pixnews.util.strictmode.isUntaggedSocketViolation
+import ru.pixnews.util.strictmode.setupViolationListener
 import javax.inject.Inject
 
 @Suppress("MagicNumber")
@@ -22,33 +23,25 @@ class DebugStrictModeInitializer @Inject constructor(logger: Logger) : Initializ
 
     override fun init() {
         logger.v { "Setting up StrictMode" }
-        val threadExecutor = Executors.newSingleThreadExecutor()
         StrictMode.setThreadPolicy(
             Builder()
                 .detectAll()
-                .also { builder ->
-                    if (Build.VERSION.SDK_INT >= 28) {
-                        builder.penaltyListener(threadExecutor) { violation ->
-                            logger.e(violation) { "ThreadPolicy violation" }
-                        }
-                    } else {
-                        builder.penaltyLog()
-                    }
-                }
+                .setupViolationListener(
+                    logger = logger,
+                    policy = LOG,
+                    logAllowListViolation = false,
+                )
                 .build(),
         )
         StrictMode.setVmPolicy(
             VmPolicy.Builder()
                 .detectAll()
-                .also { builder ->
-                    if (Build.VERSION.SDK_INT >= 28) {
-                        builder.penaltyListener(threadExecutor) { violation ->
-                            logger.e(violation) { "VmPolicy violation" }
-                        }
-                    } else {
-                        builder.penaltyLog()
-                    }
-                }
+                .setupViolationListener(
+                    logger = logger,
+                    policy = LOG,
+                    logAllowListViolation = false,
+                    allowList = listOf(isUntaggedSocketViolation),
+                )
                 .build(),
         )
 
---
+++ app/src/main/kotlin/ru/pixnews/util/strictmode/ViolationListener.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2024, the Pixnews project authors and contributors. Please see the AUTHORS file for details.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package ru.pixnews.util.strictmode
+
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.os.StrictMode
+import android.os.StrictMode.OnThreadViolationListener
+import android.os.StrictMode.OnVmViolationListener
+import android.os.strictmode.Violation
+import androidx.annotation.RequiresApi
+import co.touchlab.kermit.Logger
+import ru.pixnews.util.strictmode.ViolationListener.Companion.createThreadExecutor
+import ru.pixnews.util.strictmode.ViolationPolicy.FAIL
+import ru.pixnews.util.strictmode.ViolationPolicy.IGNORE
+import ru.pixnews.util.strictmode.ViolationPolicy.LOG
+import java.util.concurrent.Executor
+
+internal fun StrictMode.VmPolicy.Builder.setupViolationListener(
+    logger: Logger,
+    policy: ViolationPolicy = LOG,
+    logAllowListViolation: Boolean = false,
+    allowList: List<Violation.() -> Boolean> = emptyList(),
+): StrictMode.VmPolicy.Builder = if (VERSION.SDK_INT >= VERSION_CODES.P) {
+    val listener = ViolationListener(logger, policy, logAllowListViolation, allowList)
+    this.penaltyListener(createThreadExecutor(), listener)
+} else {
+    if (policy != IGNORE) {
+        this.penaltyLog()
+    } else {
+        this
+    }
+}
+
+internal fun StrictMode.ThreadPolicy.Builder.setupViolationListener(
+    logger: Logger,
+    policy: ViolationPolicy = LOG,
+    logAllowListViolation: Boolean = false,
+    allowList: List<Violation.() -> Boolean> = emptyList(),
+): StrictMode.ThreadPolicy.Builder = if (VERSION.SDK_INT >= VERSION_CODES.P) {
+    val listener = ViolationListener(logger, policy, logAllowListViolation, allowList)
+    this.penaltyListener(createThreadExecutor(), listener)
+} else {
+    if (policy != IGNORE) {
+        this.penaltyLog()
+    } else {
+        this
+    }
+}
+
+@RequiresApi(VERSION_CODES.P)
+internal class ViolationListener(
+    private val logger: Logger,
+    private val policy: ViolationPolicy = LOG,
+    private val logAllowListViolation: Boolean = false,
+    private val allowList: List<Violation.() -> Boolean> = emptyList(),
+) : OnVmViolationListener, OnThreadViolationListener {
+    override fun onVmViolation(violation: Violation) {
+        onViolation("VM", violation)
+    }
+
+    override fun onThreadViolation(violation: Violation) {
+        onViolation("Thread", violation)
+    }
+
+    private fun onViolation(
+        type: String,
+        violation: Violation,
+    ) {
+        if (violation.shouldSkip()) {
+            if (logAllowListViolation) {
+                logger.e { "Skipping $type violation `$violation: ${violation.message}`" }
+            }
+            return
+        }
+        when (policy) {
+            LOG -> logger.e(violation) { "$type violation" }
+            FAIL -> throw StrictModeViolationException(violation)
+            IGNORE -> Unit
+        }
+    }
+
+    private fun Violation.shouldSkip(): Boolean = allowList.any { it() }
+
+    companion object {
+        fun createThreadExecutor(): Executor = Executor {
+            it.run()
+        }
+    }
+}
+
+class StrictModeViolationException(throwable: Throwable) : RuntimeException(throwable)
+
+enum class ViolationPolicy {
+    IGNORE,
+    LOG,
+    FAIL,
+}

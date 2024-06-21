e866c5986b90558c780ce312ae48932b8d101d25
Alexey Illarionov <alexey@0xdc.ru>
2024-06-21 09:20:57 +0300

    Update temporarily datbase test

--- feature/calendar/data/src/test/kotlin/sync/IgdbGameModeSyncServiceTestNewRoom.kt
+++ feature/calendar/data/src/test/kotlin/sync/IgdbGameModeSyncServiceTestNewRoom.kt
@@ -7,60 +7,94 @@
 
 package ru.pixnews.feature.calendar.data.sync
 
+import android.content.ContextWrapper
 import androidx.room.Room
 import androidx.room.RoomDatabase.JournalMode.WRITE_AHEAD_LOGGING
-import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import co.touchlab.kermit.LoggerConfig
 import co.touchlab.kermit.Severity
-import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import kotlinx.coroutines.CloseableCoroutineDispatcher
+import kotlinx.coroutines.asCoroutineDispatcher
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.api.io.TempDir
 import ru.pixnews.foundation.database.PixnewsDatabase
 import ru.pixnews.foundation.database.PixnewsDatabase_Impl
+import ru.pixnews.library.test.MainCoroutineExtension
 import ru.pixnews.library.test.TestingLoggers
+import ru.pixnews.wasm.sqlite.driver.WasmSQLiteDriver
+import ru.pixnews.wasm.sqlite.open.helper.common.api.Locale
+import ru.pixnews.wasm.sqlite.open.helper.graalvm.GraalvmSqliteEmbedder
 import java.io.File
+import java.util.concurrent.Executors
 import co.touchlab.kermit.Logger as KermitLogger
 
 class IgdbGameModeSyncServiceTestNewRoom {
     val logger = TestingLoggers.consoleLogger
+    val mockContext = ContextWrapper(null)
+
+    @JvmField
+    @RegisterExtension
+    var coroutinesExt: MainCoroutineExtension = MainCoroutineExtension()
 
     @TempDir
     lateinit var tempDir: File
     lateinit var db: PixnewsDatabase
+    lateinit var queryCoroutineContext: CloseableCoroutineDispatcher
 
-    @BeforeEach
-    fun createDb() {
-        @Suppress("UnusedPrivateProperty")
+    suspend fun beforeEach() {
         val dbLogger = KermitLogger(
             config = object : LoggerConfig by logger.config {
                 override val minSeverity: Severity = Severity.Info
             },
-            "Sqlite",
+            "WasmSqlite",
         )
-
         val dbFile = File(tempDir, "db.sqlite")
+        val driver = WasmSQLiteDriver(GraalvmSqliteEmbedder) {
+            logger = SqliteLogger(dbLogger)
+            openParams {
+                locale = Locale("ru_RU")
+            }
+            debug {
+                logSqlTime = true
+            }
+        }
+
+        // or newSingleThreadContext("RoomDatabase")
+        queryCoroutineContext = Executors.newScheduledThreadPool(
+            1,
+            driver.runtime.managedThreadFactory,
+        ).asCoroutineDispatcher()
 
         db = Room.databaseBuilder(
             name = dbFile.absolutePath,
             factory = ::PixnewsDatabase_Impl,
+            context = mockContext,
         )
             .setJournalMode(WRITE_AHEAD_LOGGING)
             // .createFromAsset("pixnews.db")
-            .setDriver(BundledSQLiteDriver())
+            .setDriver(driver)
+            .setQueryCoroutineContext(queryCoroutineContext)
+            .allowMainThreadQueries()
             .build()
     }
 
-    @AfterEach
-    fun closeDb() {
+    suspend fun afterEach() {
         db.close()
+        queryCoroutineContext.close()
     }
 
     @Test
-    fun dbTest() = runTest {
-        // TODO
-        // val gameMode = db.gameModeNameDao().getById(1)
-        // logger.i { "gem mode: $gameMode" }
+    fun dbTest() = runSuspendDbTest {
+        val gameMode = db.gameModeNameDao().getById(1)
+        logger.i { "gem mode: $gameMode" }
+    }
+
+    private fun runSuspendDbTest(block: suspend () -> Unit) = coroutinesExt.runTest {
+        beforeEach()
+        try {
+            block()
+        } finally {
+            afterEach()
+        }
     }
 }
